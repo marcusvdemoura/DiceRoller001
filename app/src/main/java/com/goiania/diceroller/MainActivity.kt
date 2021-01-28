2a0628b6ae84adfcc0f97add92c62be3fea01445
marcusvdemoura <marcusdemoura@outlook.com>
2021-01-28 12:05:08 +0000

    1st

---
+++ app/src/main/java/com/goiania/diceroller/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.goiania.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton:Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{ rollDice() }
+
+
+        Log.i("DEMO", getString(R.string.Log_message))
+
+    } //  /onCreate
+
+    private fun rollDice(){
+
+        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        Log.i("DEMO", "Button Clicked to roll the dice")
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        //resultText.text = getString(R.string.SCREEN_MESSAGE_1)
+
+        val randomInt = (1..6).random()
+
+        resultText.text = randomInt.toString()
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt){
+
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+
+        }
+
+
+
+        diceImage.setImageResource(drawableResource)
+
+    } // /rollDice
+
+
+}// /MainActivity
